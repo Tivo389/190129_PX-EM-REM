4e70119f6b63d7fa12783384be89495e1e8f8632
Shun Miyaki <shunmiyaki@hotmail.com>
2019-01-30 23:11:58 -0800

    initialAdjustments complete

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <section>
+        <div className="paragraphSet">
+          <p>My grandfather's clock was too large for the shelf</p>
+          <p>So it stood ninety years on the floor</p>
+          <p>It was taller by half than the old man himself</p>
+          <p>Though it weighed not a pennyweight more</p>
+        </div>
+        <div className="paragraphSet">
+          <p>It was bought on the morn of the day that he was born</p>
+          <p>And was always his treasure and pride</p>
+          <p>But it stopped, short never to go again</p>
+          <p>When the old man died</p>
+        </div>
+        <div className="paragraphSet">
+          <p>Ninety years without slumbering</p>
+          <p>His life seconds numbering</p>
+          <p>It stopped, short never to go again</p>
+          <p>When the old man died</p>        
+        </div>
+      </section>
     );
   }
 }
