f4bc92217c39574c34ec9d50de8be6b0405c2a60
Shun Miyaki <shunmiyaki@hotmail.com>
2019-02-04 23:29:54 -0800

    measurements added, probably make a em and rem version next.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <section>
-        <div className="paragraphSet">
+      <section className="paragraphContainer">
+        <code>
+          <span className="blockC"></span>
+          font-size: 16px&ensp;/&ensp;
+          <span className="blockM"></span>
+          line-height: 1.35
+        </code>
+        <div className="paragraphBlock">
           <p>My grandfather's clock was too large for the shelf</p>
           <p>So it stood ninety years on the floor</p>
           <p>It was taller by half than the old man himself</p>
           <p>Though it weighed not a pennyweight more</p>
         </div>
-        <div className="paragraphSet">
+        <div className="paragraphBlock">
           <p>It was bought on the morn of the day that he was born</p>
           <p>And was always his treasure and pride</p>
           <p>But it stopped, short never to go again</p>
           <p>When the old man died</p>
         </div>
-        <div className="paragraphSet">
+        <div className="paragraphBlock">
           <p>Ninety years without slumbering</p>
           <p>His life seconds numbering</p>
           <p>It stopped, short never to go again</p>
